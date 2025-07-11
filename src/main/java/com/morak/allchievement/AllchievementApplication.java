f36dc4d816e05bcd8628a7b0d9eb531c19caea16
MrMOrak <MarcusOrak@hotmail.de>
2025-07-12 01:37:10 +0200

    initial commit

---
+++ src/main/java/com/morak/allchievement/AllchievementApplication.java
@@ -0,0 +1,13 @@
+package com.morak.allchievement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllchievementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllchievementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/morak/allchievement/AllchievementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.morak.allchievement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllchievementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
